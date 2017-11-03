283e0343855780bbe2197541d297891ca1cf59cc
Goichi Hirakawa <gooichi@gyazsquare.com>
2017-11-03 20:01:17 +0900

    refs #14 Add NSString searching methods

--- FoundationGSLAdditions/NSStringGSLAdditions.h
+++ FoundationGSLAdditions/NSStringGSLAdditions.h
@@ -14,7 +14,17 @@ NS_ASSUME_NONNULL_BEGIN
 - (BOOL)gsl_hasPrefixCaseInsensitive:(nullable NSString *)aString;
 - (BOOL)gsl_hasSuffixCaseInsensitive:(nullable NSString *)aString;
 
+- (BOOL)gsl_containsString:(NSString *)str;
+- (BOOL)gsl_localizedCaseInsensitiveContainsString:(NSString *)str;
+
+- (BOOL)gsl_localizedStandardContainsString:(NSString *)str;
+- (NSRange)gsl_localizedStandardRangeOfString:(NSString *)str;
+
 @property (readonly) CGFloat gsl_CGFloatValue;
+
+@property (readonly, copy) NSString *gsl_localizedUppercaseString API_AVAILABLE(macos(10.8), ios(6.0), watchos(2.0), tvos(9.0));
+@property (readonly, copy) NSString *gsl_localizedLowercaseString API_AVAILABLE(macos(10.8), ios(6.0), watchos(2.0), tvos(9.0));
+@property (readonly, copy) NSString *gsl_localizedCapitalizedString API_AVAILABLE(macos(10.8), ios(6.0), watchos(2.0), tvos(9.0));
 
 @end
 
--- FoundationGSLAdditions/NSStringGSLAdditions.m
+++ FoundationGSLAdditions/NSStringGSLAdditions.m
@@ -21,12 +21,71 @@
     return (aString && ([self rangeOfString:aString options:(NSCaseInsensitiveSearch | NSBackwardsSearch | NSAnchoredSearch)].length > 0));
 }
 
+- (BOOL)gsl_containsString:(NSString *)str {
+    if (@available(macOS 10.10, iOS 8.0, watchOS 2.0, tvOS 9.0, *)) {
+        return [self containsString:str];
+    } else {
+        return ([self rangeOfString:str].location != NSNotFound);
+    }
+}
+
+- (BOOL)gsl_localizedCaseInsensitiveContainsString:(NSString *)str {
+    if (@available(macOS 10.10, iOS 8.0, watchOS 2.0, tvOS 9.0, *)) {
+        return [self localizedCaseInsensitiveContainsString:str];
+    } else {
+        NSRange rangeOfReceiverToSearch = NSMakeRange(0, self.length);
+        return ([self rangeOfString:str options:NSCaseInsensitiveSearch range:rangeOfReceiverToSearch locale:NSLocale.currentLocale].location != NSNotFound);
+    }
+}
+
+- (BOOL)gsl_localizedStandardContainsString:(NSString *)str {
+    if (@available(macOS 10.11, iOS 9.0, watchOS 2.0, tvOS 9.0, *)) {
+        return [self localizedStandardContainsString:str];
+    } else {
+        NSRange rangeOfReceiverToSearch = NSMakeRange(0, self.length);
+        return ([self rangeOfString:str options:(NSCaseInsensitiveSearch | NSDiacriticInsensitiveSearch) range:rangeOfReceiverToSearch locale:NSLocale.currentLocale].location != NSNotFound);
+    }
+}
+
+- (NSRange)gsl_localizedStandardRangeOfString:(NSString *)str {
+    if (@available(macOS 10.11, iOS 9.0, watchOS 2.0, tvOS 9.0, *)) {
+        return [self localizedStandardRangeOfString:str];
+    } else {
+        NSRange rangeOfReceiverToSearch = NSMakeRange(0, self.length);
+        return [self rangeOfString:str options:(NSCaseInsensitiveSearch | NSDiacriticInsensitiveSearch) range:rangeOfReceiverToSearch locale:NSLocale.currentLocale];
+    }
+}
+
 - (CGFloat)gsl_CGFloatValue {
 #if CGFLOAT_IS_DOUBLE
     return self.doubleValue;
 #else
     return self.floatValue;
 #endif
+}
+
+- (NSString *)gsl_localizedUppercaseString {
+    if (@available(macOS 10.11, iOS 9.0, watchOS 2.0, tvOS 9.0, *)) {
+        return self.localizedUppercaseString;
+    } else {
+        return [self uppercaseStringWithLocale:NSLocale.currentLocale];
+    }
+}
+
+- (NSString *)gsl_localizedLowercaseString {
+    if (@available(macOS 10.11, iOS 9.0, watchOS 2.0, tvOS 9.0, *)) {
+        return self.localizedLowercaseString;
+    } else {
+        return [self lowercaseStringWithLocale:NSLocale.currentLocale];
+    }
+}
+
+- (NSString *)gsl_localizedCapitalizedString {
+    if (@available(macOS 10.11, iOS 9.0, watchOS 2.0, tvOS 9.0, *)) {
+        return self.localizedCapitalizedString;
+    } else {
+        return [self capitalizedStringWithLocale:NSLocale.currentLocale];
+    }
 }
 
 @end
